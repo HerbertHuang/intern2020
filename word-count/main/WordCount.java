4488d4be7c3665e590a2f0a43483698b3d226f8d
Nickxjl <978007384@qq.com>
2020-07-28 14:46:10 +0800

    second modified

--- word-count/main/WordCount.java
+++ word-count/main/WordCount.java
@@ -2,35 +2,16 @@ package cmsoft1;
 /*
 多线程读取多个文本文件，并统计文件内单词数量，写入到一个结果文件中
  */
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class WordCount {
     final static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //文件路径
         File f = new File("src/files");
         final List<File> filePathsList = new ArrayList<File>();
@@ -41,149 +22,102 @@ public class WordCount {
         }
 
         //创建线程池
-        ExecutorService pool = Executors.newFixedThreadPool(10);
+        ExecutorService pool = Executors.newFixedThreadPool(filePaths.length);
         //阻塞队列
         BlockingQueue<Future<Map<String, FileInputStream>>> queue =
                 new ArrayBlockingQueue<Future<Map<String, FileInputStream>>>(100);
-
+        List<Future<List<String>>> futures = new ArrayList<>();
         System.out.println("-------------文件读、写任务开始时间：" + sdf.format(new Date()));
         for (int i = 0; i < filePathsList.size(); i++) {
             File temp = filePathsList.get(i);
             //提交执行
-            Future<Map<String,FileInputStream>> future = pool.submit(new FileRead(temp));
-            queue.add(future);
-            pool.execute(new FileWrite(queue));
+            Future<List<String>> future = pool.submit(new FileRead(temp));
+            futures.add(future);
         }
-        System.out.println("-------------文件读、写任务结束时间：" + sdf.format(new Date()));
+        List<String> allWords = new ArrayList<>();
+        for (Future<List<String>> future : futures) {
+            try {
+                List<String> splits = future.get(10, TimeUnit.SECONDS);
+                allWords.addAll(splits);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("-------------文件读任务结束时间：" + sdf.format(new Date()));
         pool.shutdown();
+
+        //统计词频并写入文件
+        Map<String, AtomicInteger> wordCount = new HashMap<>();
+        for (String word : allWords) {
+            if (!wordCount.containsKey(word)) {
+                wordCount.put(word, new AtomicInteger(1));
+            } else {
+                wordCount.get(word).incrementAndGet();
+            }
+        }
+        //打印结果
+        System.out.println(wordCount);
+        BufferedWriter bufferedWriter = null;
+        try {
+            FileWriter fileWriter = new FileWriter("src/result1.txt");
+            bufferedWriter = new BufferedWriter(fileWriter);
+            Set<Map.Entry<String, AtomicInteger>> countSet = wordCount.entrySet();
+            for (Map.Entry<String, AtomicInteger> entry : countSet) {
+               bufferedWriter.write(entry.getKey() + " : " + entry.getValue() + " 个 " + '\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 
 
-
-
     // 文件读线程
-    static class FileRead implements Callable<Map<String, FileInputStream>>
-    {
+    static class FileRead implements Callable<List<String>> {
 
-        private File file;
+        private final File file;
         private FileInputStream fis = null;
-        private Map<String, FileInputStream> fileMap = new HashMap<String, FileInputStream>();
 
-
-
-        public FileRead( File file)
-        {
-
+        public FileRead(File file) {
             this.file = file;
         }
 
         @Override
-        public Map<String, FileInputStream> call() throws Exception
-        {
-            System.out.println(Thread.currentThread().getName() + " 线程开始读取文件 ：" + file.getName() + " ,时间为 "+ sdf.format(new Date()));
+        public List<String> call() throws Exception {
+            List<String> result = new ArrayList<>();
+            System.out.println(Thread.currentThread().getName() + " 线程开始读取文件 ：" + file.getName() + " ,时间为 " + sdf.format(new Date()));
             fis = new FileInputStream(file);
-            fileMap.put(file.getName(), fis);
-            System.out.println(Thread.currentThread().getName() + " 线程读取文件 ：" + file.getName() + " 完毕"  + " ,时间为 "+ sdf.format(new Date()));
-            return fileMap;
-
-        }
-
-    }
-
-
-    // 文件写线程
-   static class FileWrite implements Runnable
-    {
-        private String fileName = "";
-        private BlockingQueue<Future<Map<String, FileInputStream>>> queue;
-        private FileInputStream fis = null;
-        private File dirFile = null;
-
-        private BufferedReader br = null;
-        private InputStreamReader isr = null;
-        private FileWriter fw = null;
-        private BufferedWriter bw = null;
-        private Map<String, Integer> countMap = new HashMap<>();
-
-        public FileWrite (BlockingQueue<Future<Map<String,FileInputStream>>> queue)
-        {
-            this.queue = queue;
-        }
-
-        @Override
-        public  void run()
-        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
             try {
-                    wordCountWrite();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
-        private  void wordCountWrite() throws InterruptedException, ExecutionException {
-            Future<Map<String, FileInputStream>> future = queue.take();
-            Map<String, FileInputStream> map = future.get(); //获得读取到<文件（名）,文件输入流>
-
-            Set<String> set = map.keySet();
-            for (Iterator<String> iter = set.iterator(); iter.hasNext();) {
-                fileName = iter.next().toString();
-                fis = map.get(fileName);
-                System.out.println(Thread.currentThread().getName() + " 线程开始写文件 ：" + fileName  + " ,时间为 "+ sdf.format(new Date()));
-                try {
-                    isr = new InputStreamReader(fis, "utf-8");
-                    br = new BufferedReader(isr);
-                    //统计当前文本单词数量
-                    String line = "";
-                    while((line = br.readLine()) != null){
-                        String[] ss = line.split("\\s");
-                        for(String s : ss){
-                            if(countMap.containsKey(s)){
-                                countMap.put(s, countMap.get(s) + 1);
-                            }else{
-                                countMap.put(s, 1);
-                            }
-                        }
-                    }
-                    write();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        bw.close();
-                        br.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] split = line.split("\\s");
+                    result.addAll(Arrays.asList(split));
                 }
-            }
-        }
-
-        private  void write() throws IOException {
-            FileWriter fw = new FileWriter("src/result1.txt",true);
-            bw = new BufferedWriter(fw);
-            synchronized (fw){
-                Set<Map.Entry<String,Integer>> countSet = countMap.entrySet();
-                int count = 0;
-                //结果写入新文件
-                for(Map.Entry<String, Integer> entry : countSet){
-                    //System.out.println(entry);
-                    if(count == 0) {
-                        bw.write(fileName + " 词频统计: " + '\n');
-                    }
-                    bw.write(entry.getKey()+" : "+entry.getValue()+" 个 "+'\n');
-                    count++;
-
-                }
-                System.out.println(Thread.currentThread().getName()+" : "+ fileName +" 统计完毕");
+            } catch (IOException e) {
+                throw new IOException(String.format("%s read failed", file.getName()), e);
             }
 
+            System.out.println(Thread.currentThread().getName() + " 线程读取文件 ：" + file.getName() + " 完毕" + " ,时间为 " + sdf.format(new Date()));
+            return result;
 
         }
     }
 }
+
+
+
+
 
 
 
